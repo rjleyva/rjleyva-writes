34fcbc59c689fd7c3c233a6ad66c30861a619424
RJ Leyva <244540676+rjleyva@users.noreply.github.com>
2025-12-01 05:48:30 -0500

    chore(vite): setup server with hmr

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+  server: {
+    port: 3000,
+    strictPort: true,
+    open: true,
+    hmr: {
+      port: 3000,
+    },
+  },
+});
