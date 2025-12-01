8a9349d1010760a06f54b00deb7e2e55d64ddd1b
RJ Leyva <244540676+rjleyva@users.noreply.github.com>
2025-12-01 08:49:04 -0500

    feat(ui): add Hero component
    Include:
    - load hero.module.css

---
+++ src/components/ui/Hero/Hero.tsx
@@ -0,0 +1,16 @@
+import type React from 'react'
+import styles from './hero.module.css'
+
+interface HeroProps {
+  heroTagline: string
+}
+
+const Hero = ({ heroTagline }: HeroProps): React.JSX.Element => {
+  return (
+    <div className={styles['hero']} aria-labelledby="hero-heading">
+      <h1 className="title-large-gradient">{heroTagline}</h1>
+    </div>
+  )
+}
+
+export default Hero
