bbb9fbf3155c0f0ec375acbb7448ab2113eda88e
RJ Leyva <244540676+rjleyva@users.noreply.github.com>
2025-12-01 05:50:12 -0500

    chore(vite): setup base and import @ alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "url";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +11,12 @@ export default defineConfig({
       },
     }),
   ],
+  base: "/",
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   server: {
     port: 3000,
     strictPort: true,
