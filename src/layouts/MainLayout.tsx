5d2794bf276f273e064051b98f12dc80c21a6046
RJ Leyva <244540676+rjleyva@users.noreply.github.com>
2025-12-01 08:27:49 -0500

    feat(main-layout): render Header component

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,12 @@
 import type React from 'react'
 import { Outlet } from 'react-router'
+import Header from '@/components/ui/Header/Header'
 import styles from './home-layout.module.css'
 
 const MainLayout = (): React.JSX.Element => {
   return (
     <main id="main" className={styles['main-layout']}>
+      <Header />
       <Outlet />
     </main>
   )
