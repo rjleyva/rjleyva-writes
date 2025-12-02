2cadee162561795a6a94708826cea0582b8a5a82
RJ Leyva <244540676+rjleyva@users.noreply.github.com>
2025-12-02 15:39:58 -0500

    chore(vite): setup vite-bundle-analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,34 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { fileURLToPath, URL } from "url";
+import { fileURLToPath, URL } from 'url'
+import { defineConfig } from 'vite'
+import { analyzer } from 'vite-bundle-analyzer'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
+        plugins: [['babel-plugin-react-compiler']]
+      }
     }),
+    analyzer({
+      analyzerMode: 'static',
+      openAnalyzer: true,
+      fileName: 'bundle-report.html'
+    })
   ],
-  base: "/",
+  base: '/',
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
   server: {
     port: 3000,
     strictPort: true,
     open: true,
     hmr: {
-      port: 3000,
-    },
-  },
-});
+      port: 3000
+    }
+  }
+})
