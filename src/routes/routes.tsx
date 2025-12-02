b06413aa2cb9a05ba68fb4117d5e3196e2b42b76
RJ Leyva <244540676+rjleyva@users.noreply.github.com>
2025-12-02 13:51:09 -0500

    feat(routes): update routing
    Includes:
    - BlogPage
    - BlogPosts

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,5 +1,7 @@
 import type { RouteObject } from 'react-router'
-import Home from '@/pages/Home'
+import BlogPosts from '@/components/ui/BlogPosts/BlogPosts'
+import BlogPage from '@/pages/blog/BlogPage'
+import HomePage from '@/pages/home/HomePage'
 import MainLayout from '../layouts/MainLayout'
 
 const routes: RouteObject[] = [
@@ -8,7 +10,15 @@ const routes: RouteObject[] = [
     children: [
       {
         path: '/',
-        element: <Home />
+        element: <HomePage />
+      },
+      {
+        path: '/blog',
+        element: <BlogPosts />
+      },
+      {
+        path: '/blog/:topic/:slug',
+        element: <BlogPage />
       }
     ]
   }
