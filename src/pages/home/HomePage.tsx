792ad9a2d3dda136fbd1a665673412384c01b43a
RJ Leyva <244540676+rjleyva@users.noreply.github.com>
2025-12-03 05:07:52 -0500

    feat(home): render SeeAllPostsLink component

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,12 +1,14 @@
 import type React from 'react'
 import Hero from '@/components/ui/Hero/Hero'
 import RecentPost from '@/components/ui/RecentPost/RecentPost'
+import SeeAllPostsLink from '@/components/ui/SeeAllPostsLink/SeeAllPostsLink'
 
 const App = (): React.JSX.Element => {
   return (
     <section id="home-content">
       <Hero heroTagline="Welcome to my blog! I'm RJ, and here I document my web development insights through writing." />
       <RecentPost />
+      <SeeAllPostsLink />
     </section>
   )
 }
