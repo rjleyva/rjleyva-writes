dbafd7ee4cd07d801ac3818e546f6c89eb437ef7
RJ Leyva <244540676+rjleyva@users.noreply.github.com>
2025-12-04 16:13:42 -0500

    feat(blog-posts): add support topic filtering
    Includes:
    - render BlogPostsSidebar
    - render BlogPostsHeader
    - render BlogPostsGrid
    - updated layout to use flexbox

--- src/components/ui/BlogPosts/BlogPosts.tsx
+++ src/components/ui/BlogPosts/BlogPosts.tsx
@@ -1,27 +1,53 @@
 import type React from 'react'
+import { useMemo, useState } from 'react'
 import { useGetPosts } from '@/hooks/useBlog'
-import BlogCard from '../BlogCard/BlogCard'
+import type { Post } from '@/types/post'
+import BlogPostsGrid from '../BlogPostsGrid/BlogPostsGrid'
+import BlogPostsHeader from '../BlogPostsHeader/BlogPostsHeader'
+import BlogPostsSidebar from '../BlogPostsSidebar/BlogPostsSideBar'
 import styles from './blog-posts.module.css'
 
-const BlogList = (): React.JSX.Element => {
+const BlogPosts = (): React.JSX.Element => {
   const { posts } = useGetPosts()
+  const [selectedTopic, setSelectedTopic] = useState<string | null>(null)
+
+  const postsByTopic = useMemo(() => {
+    return posts.reduce(
+      (acc, post) => {
+        const topic = post.topic
+        const existingPosts = acc[topic] ?? []
+        acc[topic] = [...existingPosts, post]
+        return acc
+      },
+      {} as Record<string, Post[]>
+    )
+  }, [posts])
+
+  const availableTopics = Object.keys(postsByTopic).sort()
+
+  const displayedPosts =
+    selectedTopic != null ? (postsByTopic[selectedTopic] ?? []) : posts
 
   return (
-    <section className={styles['blog-posts']}>
-      <h2 className="title-large-gradient">
-        A space where I share my ideas and experiences.
-      </h2>
-      {posts.length === 0 ? (
-        <p>No posts found.</p>
-      ) : (
-        <div className="blog-grid">
-          {posts.map(post => (
-            <BlogCard key={post.slug} post={post} />
-          ))}
-        </div>
-      )}
-    </section>
+    <div className={styles['blog-posts']}>
+      <BlogPostsSidebar
+        selectedTopic={selectedTopic}
+        availableTopics={availableTopics}
+        postsByTopic={postsByTopic}
+        posts={posts}
+        onTopicSelect={setSelectedTopic}
+      />
+
+      <main className={styles['blog-posts__content']}>
+        <BlogPostsHeader selectedTopic={selectedTopic} />
+
+        <BlogPostsGrid
+          displayedPosts={displayedPosts}
+          onTopicSelect={setSelectedTopic}
+        />
+      </main>
+    </div>
   )
 }
 
-export default BlogList
+export default BlogPosts
