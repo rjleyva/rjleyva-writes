e60ce2dcb2e65a8db74ad78d307084199b93ee12
RJ Leyva <244540676+rjleyva@users.noreply.github.com>
2025-12-03 20:56:11 -0500

    chore(vite): setup chunking and minification

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,36 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: id => {
+          if (
+            id.includes('react') ||
+            id.includes('react-dom') ||
+            id.includes('react-router')
+          ) {
+            return 'react-vendor'
+          }
+          if (
+            id.includes('unified') ||
+            id.includes('remark-') ||
+            id.includes('rehype-')
+          ) {
+            return 'markdown-vendor'
+          }
+          if (id.includes('modern-normalize')) {
+            return 'ui-vendor'
+          }
+        },
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    },
+    minify: 'esbuild'
+  },
   server: {
     port: 3000,
     strictPort: true,
