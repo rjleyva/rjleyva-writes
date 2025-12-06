16ae836d36aac9126d9ea27f74a7e86e8d917757
RJ Leyva <244540676+rjleyva@users.noreply.github.com>
2025-12-06 11:33:55 -0500

    refactor(home-page): integrate usePageTitle hook

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,11 +1,18 @@
 import type React from 'react'
+import { Helmet } from '@dr.pogodin/react-helmet'
 import Hero from '@/components/ui/Hero/Hero'
 import RecentPost from '@/components/ui/RecentPost/RecentPost'
 import SeeAllPostsLink from '@/components/ui/SeeAllPostsLink/SeeAllPostsLink'
+import { usePageTitle } from '@/hooks/usePageTitle'
 
-const App = (): React.JSX.Element => {
+const HomePage = (): React.JSX.Element => {
+  const pageTitle = usePageTitle("RJ Leyva's Home Page")
+
   return (
     <section id="home-content">
+      <Helmet>
+        <title>{pageTitle}</title>
+      </Helmet>
       <Hero heroTagline="Welcome to my blog! I'm RJ, and here I document my web development insights through writing." />
       <RecentPost />
       <SeeAllPostsLink />
@@ -13,4 +20,4 @@ const App = (): React.JSX.Element => {
   )
 }
 
-export default App
+export default HomePage
