4cd72b20e45f6223cdfab0ad5095771937d945ab
RJ Leyva <244540676+rjleyva@users.noreply.github.com>
2025-12-06 22:29:47 -0500

    feat(main): use ApplicationErrorBoundary component

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { HelmetProvider } from '@dr.pogodin/react-helmet'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router'
+import { ApplicationErrorBoundary } from './components/ApplicationErrorBoundary/ApplicationErrorBoundary'
 import { ThemeProvider } from './contexts/ThemeProvider'
 import routes from './routes/routes'
 import './utils/faviconManager'
@@ -19,10 +20,12 @@ if (!(rootElement instanceof HTMLElement)) {
 
 createRoot(rootElement).render(
   <StrictMode>
-    <HelmetProvider>
-      <ThemeProvider>
-        <RouterProvider router={router} />
-      </ThemeProvider>
-    </HelmetProvider>
+    <ApplicationErrorBoundary>
+      <HelmetProvider>
+        <ThemeProvider>
+          <RouterProvider router={router} />
+        </ThemeProvider>
+      </HelmetProvider>
+    </ApplicationErrorBoundary>
   </StrictMode>
 )
