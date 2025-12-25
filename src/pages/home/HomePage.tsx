90dcfe16bd9a4a8401d441eedad8f6c1139c0b2a
RJ Leyva <244540676+rjleyva@users.noreply.github.com>
2025-12-25 11:10:54 -0500

    refactor(home-page): remove Helmet usage

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,18 +1,14 @@
 import type React from 'react'
-import { Helmet } from '@dr.pogodin/react-helmet'
 import Hero from '@/components/ui/Hero/Hero'
 import RecentPost from '@/components/ui/RecentPost/RecentPost'
 import SeeAllPostsLink from '@/components/ui/SeeAllPostsLink/SeeAllPostsLink'
 import { usePageTitle } from '@/hooks/usePageTitle'
 
 const HomePage = (): React.JSX.Element => {
-  const pageTitle = usePageTitle("RJ Leyva's Home Page")
+  usePageTitle("RJ Leyva's Home Page")
 
   return (
     <section id="home-content">
-      <Helmet>
-        <title>{pageTitle}</title>
-      </Helmet>
       <Hero heroTagline="Welcome to my blog! I'm RJ, and here I document my web development insights through writing." />
       <RecentPost />
       <SeeAllPostsLink />
